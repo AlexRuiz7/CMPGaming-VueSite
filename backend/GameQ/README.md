b053657ad9e136762b39c769e2116dd0106a1bce
Álex <alex-r-b@hotmail.com>
2019-11-22 15:21:00 +0100

    servers.php documented. Added doxygen support

---
+++ backend/GameQ/README.md
@@ -0,0 +1,51 @@
+#GameQ Version 3
+[![Build Status](https://travis-ci.org/Austinb/GameQ.svg?branch=v3&style=flat-square)](https://travis-ci.org/Austinb/GameQ)
+[![Coverage Status](https://coveralls.io/repos/Austinb/GameQ/badge.svg?branch=v3&style=flat-square)](https://coveralls.io/r/Austinb/GameQ?branch=v3)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/Austinb/GameQ/badges/quality-score.png?b=v3&style=flat-square)](https://scrutinizer-ci.com/g/Austinb/GameQ/?branch=v3)
+[![License](https://img.shields.io/badge/license-LGPL-blue.svg?style=flat-square)](https://packagist.org/packages/austinb/gameq)
+[![Dependency Status](https://www.versioneye.com/user/projects/54d6a7d43ca0845f67000002/badge.svg?style=flat-square)](https://www.versioneye.com/user/projects/54d6a7d43ca0845f67000002)
+
+GameQ is a PHP library that allows you to query multiple types of multiplayer game & voice servers at the same time.
+
+## Requirements
+* PHP 5.4.14+ - [Tested](https://travis-ci.org/Austinb/GameQ) in PHP 5.4, 5.5, 5.6 & [HHVM](http://hhvm.com/)
+* [Bzip2](http://www.php.net/manual/en/book.bzip2.php) - Used for A2S Compressed responses
+
+## Installation
+#### [Composer](https://getcomposer.org/)
+This method assumes you already have composer [installed](https://getcomposer.org/doc/00-intro.md) and working properly. Add `austinb/gameq` as a requirement to composer.json by using `composer require austinb/gameq:3.*@stable` or by manually adding the following to the *composer.json* file in the **require** section:
+
+```javascript
+"austinb/gameq": "3.*@stable"
+```
+
+Update your packages with `composer update` or install with `composer install`.
+
+#### Standalone Library
+Download the [latest version](https://github.com/Austinb/GameQ/releases) of the library and unpack it into your project.  Add the following to your bootstrap file:
+```php
+require_once('/path/to/src/GameQ/Autoloader.php');
+```
+The Autoloader.php file provides the same auto loading functionality as the Composer install.
+
+## Example
+```php
+$GameQ = new \GameQ\GameQ();
+$GameQ->addServer([
+    'type' => 'css',
+    'host' => '127.0.0.1:27015',
+]);
+$results = $GameQ->process();
+```
+Need more?  See [Examples](https://github.com/Austinb/GameQ/wiki/Examples-v3).
+
+## Contributing 
+ 
+Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
+
+## License
+See [LICENSE](LICENSE.lgpl) for more information
+
+Donations
+=========
+If you like this project and use it a lot please feel free to donate here: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VAU2KADATP5PU.
--- backend/servers.php
+++ backend/servers.php
@@ -5,16 +5,29 @@ require_once './config/serversInfo.php';
 require_once './Socket/Websockets.php';
 require_once './GameQ/Autoloader.php';
 
+/**
+ * CMPSocket class
+ * 
+ * Note: Do not place the files in your web server's document root -- they are
+ * not intended to be ran through a web browser or otherwise directly accessible
+ * to the world. They are intended to be ran through PHP's Command Line Interface
+ * (CLI).
+ */
 class CMPSocket extends WebSocketServer {
 
     /**
      * $SERVERS contains CMP Servers IP:PORT in JSON format
      */
-    private $GameQ;         // GameQ instance
-    private $results;       // Array
+    private $GameQ;         /** GameQ instance @var GameQ */ 
+    private $results;       /** Container for the results provided by GameQ queries @var Array */ 
 
     /**
      * Constructor
+     * 
+     * Initializes class attributes
+     *
+     * @param string $addr IP address of the websocket server
+     * @param string $port PORT of the websocket server
      */
     function __construct($addr, $port) {
         parent::__construct($addr, $port);
@@ -28,7 +41,15 @@ class CMPSocket extends WebSocketServer {
     }
 
     /**
+     * This method is called into the main processing loop, run(). 
      * 
+     * Place here any process that should happen periodically.
+     * Will happen at least once per second, but possibly more often.
+     * 
+     * Queries game servers using GameQ and sends the results to all users 
+     * connected to this socket server.
+     * 
+     * @return void
      */
     protected function tick() {
         $this->results = $this->GameQ->process();
@@ -36,7 +57,10 @@ class CMPSocket extends WebSocketServer {
     }
 
     /**
-     * 
+     * Sends a message to all the users connectes to this socket server.
+     *
+     * @param string $message
+     * @return void
      */
     private function send2All($message) {
         foreach ($this->users as $currentUser)
@@ -44,17 +68,41 @@ class CMPSocket extends WebSocketServer {
     }    
 
     /**
-     * Unused methods, but override is required
+     * Called immediately when the data is received.
+     * 
+     * Unused method, but override is required
+     * 
+     * @param User $user
+     * @param string $message
+     * @return void
      */
-    protected function process($user, $message) {} 
+    protected function process($user, $message) {}
+    
+    /**
+     * Called after the handshake response is sent to the client.
+     * 
+     * Unused method, but override is required
+     * 
+     * @param User $user
+     * @return void
+     */
     protected function connected($user) {}
+
+    /**
+     * Called after the connection is closed.
+     *
+     * Unused method, but override is required
+     * 
+     * @param User $user
+     * @return void
+     */
     protected function closed($user) {}
 
 }
 
 
 /**
- * Start socket
+ * Start socket server
  */
 $socket = new CMPSocket('localhost', '1942');
 
